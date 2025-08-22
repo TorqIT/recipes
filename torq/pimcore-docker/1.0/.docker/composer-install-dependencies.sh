1396d13274e89fea83643227e1c29d1ab4496240
Evan Jackson <evan.jackson@torqit.ca>
2025-08-22 15:50:48 -0400

    Add composer-install-dependencies.sh

---
+++ torq/pimcore-docker/1.0/.docker/composer-install-dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo Installing Composer packages...
+cd /var/www/html && runuser -u www-data -- php -d memory_limit=-1 -d xdebug.remote_enable=0 /usr/local/bin/composer install --prefer-dist --no-scripts
