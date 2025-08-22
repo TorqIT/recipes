2a0f545cbd4da363622c6960a4f345f89990f413
Evan Jackson <evan.jackson@torqit.ca>
2025-08-22 15:54:33 -0400

    Add Composer configuration to script

--- torq/pimcore-docker/1.0/.docker/composer-install-dependencies.sh
+++ torq/pimcore-docker/1.0/.docker/composer-install-dependencies.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+echo Configuring Composer...
+runuser -u www-data -- mkdir -p /var/www/.config/composer && mkdir -p /var/www/.cache/composer
+chown -R /var/www
+
 echo Installing Composer packages...
 cd /var/www/html && runuser -u www-data -- php -d memory_limit=-1 -d xdebug.remote_enable=0 /usr/local/bin/composer install --prefer-dist --no-scripts
